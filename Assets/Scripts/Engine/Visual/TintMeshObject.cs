6d8e8e1a88325f1324ac31ca46fd2baf5e251fc8
Ariel Jalkiver <thewolf359@gmail.com>
2024-04-14 12:08:25 +0300

    Rename tint

--- Assets/Scripts/Engine/Visual/TintMeshObject.cs
+++ Assets/Scripts/Engine/Visual/TintMeshObject.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class TintObject : MonoBehaviour
+public class TintMeshObject : MonoBehaviour
 {
     [SerializeField]
     [Range(0.1f, 2f)]
